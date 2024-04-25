d4eefb358dca0539d1d12483cc756920ee91412b
nikhen-s <nikhen.sanjaya2003@gmail.com>
2024-04-25 21:15:51 +0200

    Population Graph Scaffold + Parallel Coordinates draft

---
+++ website/src/sections/Graph4.js
@@ -0,0 +1,71 @@
+import React, { useEffect } from 'react'
+import './Graph4.css'
+import Plotly from 'plotly.js/dist/plotly'
+import CodersData from '../data/codersData.json'
+
+const Graph4 = () => {
+  useEffect(()=>{
+    const dataArray = Object.values(CodersData.CodingHours)
+    dataArray.forEach(value => console.log(value));
+    var data = [
+      {
+          type: 'parcoords',
+          line: {
+              color: 'blue'
+          },
+          dimensions: [
+              {
+                  range: [1, 10],
+                  label: 'Coding Hours',
+                  values: Object.values(CodersData.CodingHours)
+              },
+              {
+                  range: [1, 10],
+                  label: 'Coffee Cups Per Day',
+                  values: Object.values(CodersData.CoffeeCupsPerDay)
+              },
+              {
+                  range: [1, 5],
+                  label: 'Coffee Time',
+                  values: Object.values(CodersData.CoffeeTime)
+              },
+              {
+                  range: [1, 5],
+                  label: 'Coding Without Coffee',
+                  values: Object.values(CodersData.CodingWithoutCoffee)
+              },
+              {
+                  range: [1, 10],
+                  label: 'Coffee Solve Bugs',
+                  values: Object.values(CodersData.CoffeeSolveBugs)
+              },
+              {
+                  range: [1, 10],
+                  label: 'Gender',
+                  values: Object.values(CodersData.Gender)
+              },
+              {
+                  range: [1, 10],
+                  label: 'Age Range',
+                  values: Object.values(CodersData.AgeRange)
+              },
+              {
+                  range: [1, 10],
+                  label: 'Cluster',
+                  values: Object.values(CodersData.Cluster)
+              }
+          ]
+      }
+  ];
+  
+  Plotly.newPlot('ParallelCoordinatesGraph', data);
+  })
+  return (
+    <div className='graph4' id='graph4'>
+      <h1>Parallel Coordinates Graph</h1>
+      <div id = "ParallelCoordinatesGraph"></div>
+    </div>
+  )
+}
+
+export default Graph4
--- website/src/sections/Home.js
+++ website/src/sections/Home.js
@@ -2,6 +2,8 @@ import React from 'react';
 import '../App.css';
 import Welcome from './Welcome';
 import Footer from "./Footer";
+import Graph4 from "./Graph4"
+import Graph3 from "./Graph3";
 import Graph2 from "./Graph2";
 import Graph from "./Graph";
 
@@ -11,6 +13,8 @@ function Home() {
       <Welcome />
       <Graph />
       <Graph2 />
+      <Graph3 />
+      <Graph4 />
       <Footer/>
     </>
   );
